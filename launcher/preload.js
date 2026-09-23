01f1684e060be6b791355e238b3bbd2e12eb4f76
Gaurav Srivastava <srivastavagaurav609@gmail.com>
2026-09-23 20:18:46 +0530

    Refresh launcher host bridge

--- launcher/preload.js
+++ launcher/preload.js
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('launcherHost', {
   ready: () => ipcRenderer.invoke('launcher-ready'),
   setMode: mode => ipcRenderer.invoke('launcher-set-mode', mode),
-  setPointerInteractive: interactive => ipcRenderer.invoke('launcher-pointer', !interactive),
+  setPointerInteractive: interactive => ipcRenderer.invoke('launcher-pointer', Boolean(interactive)),
   getConfig: () => ipcRenderer.invoke('launcher-config'),
   quit: () => ipcRenderer.send('launcher-quit'),
   onSummon: callback => ipcRenderer.on('launcher-summon', () => callback()),
